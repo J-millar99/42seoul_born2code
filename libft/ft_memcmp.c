5dc2a070df691a475933420fe9b3a98dfa725680
Jaehyun Ji <jaji@c2r9s6.42seoul.kr>
2023-03-21 17:18:45 +0900

    rename root->libft

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaji <jaji@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/20 22:01:09 by jaji              #+#    #+#             */
+/*   Updated: 2023/03/20 22:01:09 by jaji             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *str1, const void *str2, size_t size)
+{
+	const unsigned char	*s1;
+	const unsigned char	*s2;
+
+	s1 = (const unsigned char *)str1;
+	s2 = (const unsigned char *)str2;
+	while (size-- > 0)
+	{
+		if (*s1 != *s2)
+			break ;
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
