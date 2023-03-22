b2fdf50cfb71f25cc6f5e2225231973a9e279e58
J-millar99 <wlwogus33@gmail.com>
2023-03-22 21:55:48 +0900

    2023-03-22

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -12,27 +12,27 @@
 
 #include "libft.h"
 
-int atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int idx;
-    int sign;
-    int result;
+	int		idx;
+	int		sign;
+	int		result;
 
-    idx = 0;
-    sign = 1;
-    result = 0;
-    while (str[idx] == 32 || (9 <= str[idx] && str[idx] <= 13))
-        ++idx;
-    if (str[idx] == '+' || str[idx] == '-')
-    {
-        if (str[idx] == '-')
-            sign = -1;
-        ++idx;
-    }
-    while ('0' <= str[idx] && str[idx] <= '9')
-    {
-        result = (result * 10) + (str[idx] - '0');
-        ++idx;
-    }
-    return (sign * result);
+	idx = 0;
+	sign = 1;
+	result = 0;
+	while (str[idx] == 32 || (9 <= str[idx] && str[idx] <= 13))
+		++idx;
+	if (str[idx] == '+' || str[idx] == '-')
+	{
+		if (str[idx] == '-')
+			sign = -1;
+		++idx;
+	}
+	while ('0' <= str[idx] && str[idx] <= '9')
+	{
+		result = (result * 10) + (str[idx] - '0');
+		++idx;
+	}
+	return (sign * result);
 }
