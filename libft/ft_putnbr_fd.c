ffe186782b2e54f6f6705f53b0c0b5e41f7f218d
J-millar99 <wlwogus33@gmail.com>
2023-03-24 00:51:17 +0900

    2023-03-24

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaji <jaji@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: jaehyji <jaehyji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 04:01:55 by jaji              #+#    #+#             */
-/*   Updated: 2023/03/22 04:48:19 by jaji             ###   ########.fr       */
+/*   Updated: 2023/03/23 19:54:27 by jaehyji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ static int	check_cnt(int n)
 	int		cnt;
 
 	cnt = 0;
-	if (n == 0)
-		cnt = 1;
 	while (n != 0)
 	{
 		n /= 10;
@@ -34,25 +32,31 @@ static int	tenpow(int cnt)
 	return (10 * tenpow(cnt - 1));
 }
 
-static void	lastzero(int n, int fd)
+static void	lastzero(int n, int cnt, int fd)
 {
-	if (n % 10 == 0)
+	if (cnt == 0)
+	{
 		write(fd, "0", 1);
+		return ;
+	}
+	while (n != 0 && cnt != 0)
+	{
+		n /= 10;
+		cnt++;
+	}
+	return ;
 }
 
 void	ft_putnbr_fd(int n, int fd)
 {
 	char	c;
 	int		cnt;
-	int		temp;
+	int		tempcnt;
+	int		tempn;
 
-	temp = n;
+	tempn = n;
 	cnt = check_cnt(n);
-	if (n == 0)
-	{
-		write(fd, "0", 1);
-		return ;
-	}
+	tempcnt = cnt;
 	if (n < 0)
 		write(fd, "-", 1);
 	while (n)
@@ -65,5 +69,5 @@ void	ft_putnbr_fd(int n, int fd)
 		n %= tenpow(cnt - 1);
 		cnt--;
 	}
-	lastzero(temp, fd);
+	lastzero(tempn, tempcnt, fd);
 }
