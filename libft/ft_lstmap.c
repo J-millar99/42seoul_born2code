08d8800fdfab0c45a902042d479e7a059c54326f
J-millar99 <wlwogus33@gmail.com>
2023-03-24 03:41:33 +0900

    2023-03-24

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jaehyji <jaehyji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 17:17:18 by jaehyji           #+#    #+#             */
-/*   Updated: 2023/03/24 02:16:48 by jaehyji          ###   ########.fr       */
+/*   Updated: 2023/03/24 03:37:41 by jaehyji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*temp_lst;
 	t_list	*start;
 
-	if (lst == NULL)
+	if (lst == NULL || f == NULL)
 		return (NULL);
 	temp_lst = (t_list *)malloc(sizeof(t_list));
 	if (temp_lst == NULL)
@@ -26,16 +26,16 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	while (lst->next != NULL)
 	{
 		temp_lst->next = (t_list *)malloc(sizeof(t_list));
-		if (temp_lst == NULL || temp_lst->next == NULL)
+		if (temp_lst->next == NULL)
 		{
 			ft_lstclear(&start, del);
 			return (NULL);
 		}
-		temp_lst->content = f(lst->content);
+		temp_lst->content = (*f)(lst->content);
 		temp_lst = temp_lst->next;
 		lst = lst->next;
 	}
-	temp_lst->content = f(lst->content);
+	temp_lst->content = (*f)(lst->content);
 	temp_lst->next = NULL;
 	return (start);
 }
