95a6977efab21d3faefa31bfaffc6b33cdf8b120
J-millar99 <wlwogus33@gmail.com>
2023-06-01 20:21:15 +0900

    2023-06-01

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,15 +6,11 @@
 /*   By: jaehyji <jaehyji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 10:28:28 by jaehyji           #+#    #+#             */
-/*   Updated: 2023/05/30 14:31:53 by jaehyji          ###   ########.fr       */
+/*   Updated: 2023/06/01 20:21:09 by jaehyji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-/*
-	str은 이차원 문자배열이므로 1차, 2차 모두 free해줄 것.
-*/
 
 int	main(int ac, char *av[])
 {
@@ -25,22 +21,7 @@ int	main(int ac, char *av[])
 	make_stack(&alst, str);
 	if (!check_sorted(alst))
 		sort_algorithm(&alst);
+	lstclear(alst);
+	free_split(str);
 	return (0);
 }
-
-/*
-	명령어 테스트케이스
-	printf("===stack a===\n");
-	for (int i = 0; i < 4; i++)
-	{
-		printf("%d\n", alst->value);
-		alst = alst->next;
-	}
-	i = 0;
-	printf("===stack b===\n");
-	for (int i = 0; i < 4; i++)
-	{
-		printf("%d\n", blst->value);
-		blst = blst->next;
-	}
-*/
