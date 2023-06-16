63b5cc9ac4e03e6fe881013157363f780d40b020
J-millar99 <wlwogus33@gmail.com>
2023-06-16 18:23:11 +0900

    2023-06-16

--- push_swap/find_value_utils.c
+++ push_swap/find_value_utils.c
@@ -6,7 +6,7 @@
 /*   By: jaehyji <jaehyji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 12:31:43 by jaehyji           #+#    #+#             */
-/*   Updated: 2023/06/15 15:17:19 by jaehyji          ###   ########.fr       */
+/*   Updated: 2023/06/16 12:13:57 by jaehyji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,7 @@ int	find_in_alst(t_node *alst, int b_val)
 }
 
 /*
+	before push,
 	The number of times the stack needs to be rotated
 	to place the value in the correct position.
 */
--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,17 +6,38 @@
 /*   By: jaehyji <jaehyji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 10:28:28 by jaehyji           #+#    #+#             */
-/*   Updated: 2023/06/12 15:27:52 by jaehyji          ###   ########.fr       */
+/*   Updated: 2023/06/16 18:20:58 by jaehyji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	leaks_func(void)
+{
+	system("leaks push_swap");
+}
+
+void	clear_all(t_node **alst, char **str)
+{
+	int		size;
+	t_node	*tmp;
+
+	size = lstsize(*alst);
+	while (size--)
+	{
+		tmp = (*alst)->next;
+		free(*alst);
+		*alst = tmp;
+	}
+	free_split(str);
+}
 
 int	main(int ac, char *av[])
 {
 	char	**str;
 	t_node	*alst;
 
+	atexit(leaks_func);
 	alst = NULL;
 	if (ac == 1)
 		exit(0);
@@ -24,5 +45,6 @@ int	main(int ac, char *av[])
 	make_stack(&alst, str);
 	if (!check_sorted(alst))
 		sort_algorithm(&alst);
+	clear_all(&alst, str);
 	return (0);
 }
