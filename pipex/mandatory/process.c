84e321bf3550a52f063550044de3997bf1dafec3
J-millar99 <wlwogus33@gmail.com>
2023-07-12 13:46:07 +0900

    2023-07-12

--- pipex/mandatory/process.c
+++ pipex/mandatory/process.c
@@ -6,26 +6,11 @@
 /*   By: jaehyji <jaehyji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 15:42:47 by jaehyji           #+#    #+#             */
-/*   Updated: 2023/07/10 19:28:36 by jaehyji          ###   ########.fr       */
+/*   Updated: 2023/07/12 13:45:59 by jaehyji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-/*
-	infile: (3)
-	outfile: (4)
-	fd[0]: read pipe fd (5)
-	fd[1]: write pipe fd (6)
-	
-	// 0 -> 3 (infile fd)
-	// 1 -> 6 (write pipe fd)
-	// now, stdin fd(0) is infile fd(3), stdout fd(1) is write pipe fd (6)
-
-	// 1 -> 4(outfile fd)
-	// 0 -> 5(read pipe fd)
-	// now, stdin fd(0) is read pipe fd(5), stdout fd(1) is outfile fd (4)
-*/
 
 void	f_process(pid_t child, t_cmd *info)
 {
