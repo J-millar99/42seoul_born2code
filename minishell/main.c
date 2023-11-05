86d20bc5e45a49d67d912ff2a62bdb3a055bb465
J-millar99 <wlwogus33@gmail.com>
2023-11-06 04:07:07 +0900

    2023-11-06

--- minishell/main.c
+++ minishell/main.c
@@ -3,26 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaehyji <jaehyji@student.42.fr>            +#+  +:+       +#+        */
+/*   By: millar <millar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/25 11:31:08 by jaehyji           #+#    #+#             */
-/*   Updated: 2023/08/25 12:11:32 by jaehyji          ###   ########.fr       */
+/*   Created: 2023/08/25 15:52:52 by jaehyji           #+#    #+#             */
+/*   Updated: 2023/11/06 03:59:35 by millar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char *argv[], char **envp)
+char	*read_command_line(char ***env)
 {
-	char	*line;
+	char			*line;
+	t_linked_list	*list;
+	t_tree_node		*root;
 
+	line = readline("minishell$ ");
+	if (line)
+	{
+		add_history(line);
+		list = lexer(line);
+		if (list == 0)
+			return (line);
+		root = parser(list, *env);
+		if (root == 0)
+			return (line);
+		free_list(list, 0);
+		run_root(root, env);
+		free_tree(root, 1);
+	}
+	else
+		shell_ctrl_d();
+	return (line);
+}
+
+int	main()
+{
+	char	**env;
+	char	*line;
+	int		o_fd[2];
+
+	env = init_setting(environ, o_fd);
 	while (1)
 	{
-		line = readline("minishell-1.0$ ");
-		if (!line)
-			exit(0);
-		add_history(line);
-		printf("%s\n", line);
-		free(line);
+		set_shell_signal();
+		line = read_command_line(&env);
+		if (line)
+			free(line);
+		else
+			break ;
+		recover_std_fd(o_fd);
 	}
+	return (0);
 }
