870d71b2faa610c89c08933a4abd272bd9b30903
J-millar99 <wlwogus33@gmail.com>
2024-02-24 17:50:51 +0900

    2024-02-24

--- cpp/Module 06/ex02/main.cpp
+++ cpp/Module 06/ex02/main.cpp
@@ -27,39 +27,35 @@ void identify(Base *p)
     B *ptr2 = dynamic_cast<B*>(p);
     C *ptr3 = dynamic_cast<C*>(p);
 
-    std::cout << "Base *p actual type: ";
+    std::cout << "Base *p actual type> ";
     if (ptr1)
-        std::cout << "A";
+        std::cout << "A: " << ptr1;
     if (ptr2)
-        std::cout << "B";
+        std::cout << "B: " << ptr2;
     if (ptr3)
-        std::cout << "C";
+        std::cout << "C: " << ptr3;
     std::cout << std::endl;
 }
 
 void identify(Base &p)
 {
-    A aRef;
-    B bRef;
-    C cRef;
-
-    std::cout << "Base &p actual type: ";
+    std::cout << "Base &p actual type> ";
     try
     {
-        aRef = dynamic_cast<A&>(p);
-        std::cout << "A";
+        A &aRef = dynamic_cast<A&>(p);
+        std::cout << "A: " << &aRef;
     } 
     catch (const std::bad_cast&){}
     try
     {
-        bRef = dynamic_cast<B&>(p);
-        std::cout << "B";
+        B &bRef = dynamic_cast<B&>(p);
+        std::cout << "B: " << &bRef;
     }
     catch (const std::bad_cast&){}
     try
     {
-        cRef = dynamic_cast<C&>(p);
-        std::cout << "C";
+        C &cRef = dynamic_cast<C&>(p);
+        std::cout << "C: " << &cRef;
     }
     catch (const std::bad_cast&){}
     std::cout << std::endl;
