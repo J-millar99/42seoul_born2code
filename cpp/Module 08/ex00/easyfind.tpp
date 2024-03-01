1536e4f5a0b039d4f85fcb8d338fb3ca8513b808
J-millar99 <wlwogus33@gmail.com>
2024-03-01 15:29:32 +0900

    2024-03-01

--- cpp/Module 08/ex00/easyfind.tpp
+++ cpp/Module 08/ex00/easyfind.tpp
@@ -12,4 +12,15 @@ typename T::iterator easyfind(T &container, int value)
         throw std::out_of_range("Value not found in container");
 }
 
+template <typename T>
+typename T::const_iterator easyfind(const T &container, int value)
+{
+    typename T::const_iterator it = std::find(container.begin(), container.end(), value);
+
+    if (it != container.end())
+        return it;
+    else
+        throw std::out_of_range("Value not found in container");
+}
+
 #endif
--- cpp/Module 08/ex00/main.cpp
+++ cpp/Module 08/ex00/main.cpp
@@ -9,9 +9,8 @@ int main()
     // Sequence container(index) : vector, list, deque, string, queue, stack, array(C++11)
     // Associative container(key - value) : map, set, unordered_map, unordered_set
 
-    int inum = 3; // finding number
+    int inum = 5; // finding number
     int arr[] = {1, 2, 3, 3, 4};
-    const int const_arr[] = {1, 2, 3, 3, 4};
     try
     {
         std::vector<int> vector(arr, arr + sizeof(arr) / sizeof(arr[0]));
@@ -43,6 +42,40 @@ int main()
     catch (const std::exception& e) 
     {
         std::cerr << "Error: " << e.what() << "(deque)" << std::endl;
+    }
+
+
+    try
+    {
+        std::vector<const int> vector(arr, arr + sizeof(arr) / sizeof(arr[0]));
+        std::vector<const int>::const_iterator result = easyfind(vector, inum);
+        std::cout << "Value found in const vector at index: " << std::distance(vector.begin(), result) << std::endl;
+    } 
+    catch (const std::exception& e) 
+    {
+        std::cerr << "Error: " << e.what() << "(const vector)" << std::endl;
+    }
+
+    try
+    {
+        std::list<const int> list(arr, arr + sizeof(arr) / sizeof(arr[0]));
+        std::list<const int>::const_iterator result = easyfind(list, inum);
+        std::cout << "Value found in const list at index: " << std::distance(list.cbegin(), result) << std::endl;
+    }
+    catch (const std::exception& e) 
+    {
+        std::cerr << "Error: " << e.what() << "(const list)" << std::endl;
+    }
+
+    try
+    {
+        std::deque<const int> deque(arr, arr + sizeof(arr) / sizeof(arr[0]));
+        std::deque<const int>::const_iterator result = easyfind(deque, inum);
+        std::cout << "Value found in const deque at index: " << std::distance(deque.cbegin(), result) << std::endl;
+    }
+    catch (const std::exception& e) 
+    {
+        std::cerr << "Error: " << e.what() << "(const deque)" << std::endl;
     }
     return 0;
 }
